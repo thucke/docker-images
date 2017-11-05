d4ce40b17748b113591885345939b396319cee30
Thomas Hucke <15112795+thucke@users.noreply.github.com>
2017-11-05 18:42:34 +0100

    Should be ready now

--- OracleDatabase/setup/install-apex.sh
+++ OracleDatabase/setup/install-apex.sh
@@ -8,10 +8,13 @@ APEX_FILE=apex_5.1.3.zip
 unzip $ORACLE_BASE/scripts/setup/$APEX_FILE -d $ORACLE_BASE/scripts/setup
 
 cd $ORACLE_BASE/scripts/setup/apex
+su -p oracle -c "cp apxchpwd.sql /u01/app/oracle/oradata/dbconfig/XE"
 su -p oracle -c "sqlplus / as sysdba <<EOF
-spool $ORACLE_BASE/scripts/setup/installApex.log
+spool /tmp/installApex.log
 @apexins.sql SYSAUX SYSAUX TEMP /i/
 @apex_epg_config.sql $ORACLE_BASE/scripts/setup
+
+set echo on
 @apxchpwd.sql
 EOF"
 
